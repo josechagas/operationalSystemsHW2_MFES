652b8cc19dff42055fb0c34483ff5cf4dda07b7f
joselucas <joselucas1994@yahoo.com.br>
2016-09-01 17:40:18 -0300

    changes on Computer.java

--- src/main/java/models/Computer.java
+++ src/main/java/models/Computer.java
@@ -34,7 +34,7 @@ public class Computer implements Runnable{
         System.out.println("Git update 2: Flavio");
         System.out.println("Git update 3: Flavio");
         System.out.println("Git update 4: Lucas");
-        System.out.println("Git update 5: Flavio");
+        System.out.println("Git update 5: Flavio alteracao para gerar o conflito");
 
         
     }
